83fb9c62208da56df4d04c538f9faae827c91d6e
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-04-23 22:26:22 +0000

    add project for OBIEE jax-ws client
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/obiee-ws-client@6544 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

---
+++ src/test/java/org/ccci/obiee/client/GhostTest.java
@@ -0,0 +1,78 @@
+package org.ccci.obiee.client;
+
+
+import org.testng.annotations.Test;
+
+import com.siebel.analytics.web.soap.v5.SAWSessionService;
+import com.siebel.analytics.web.soap.v5.SAWSessionServiceSoap;
+import com.siebel.analytics.web.soap.v5.XmlViewService;
+import com.siebel.analytics.web.soap.v5.XmlViewServiceSoap;
+import com.siebel.analytics.web.soap.v5.model.QueryResults;
+import com.siebel.analytics.web.soap.v5.model.ReportParams;
+import com.siebel.analytics.web.soap.v5.model.ReportRef;
+import com.siebel.analytics.web.soap.v5.model.XMLQueryExecutionOptions;
+import com.siebel.analytics.web.soap.v5.model.XMLQueryOutputFormat;
+
+public class GhostTest
+{
+
+    static
+    {
+        //bypass the LH proxy server, which sometimes can't connect to the server under test
+        System.setProperty("http.nonProxyHosts", "hart-*|harta|*.ccci.org");
+    }
+    
+    final static String TEST_USER = "MDREES";
+    final static String TEST_PASSWORD= "MDREES";
+    
+//    @Test
+    public void testLogonLogoff()
+    {
+        SAWSessionService sessionServiceEndpoint = new SAWSessionService();
+        SAWSessionServiceSoap sessionService = sessionServiceEndpoint.getSAWSessionServiceSoap();
+        
+        String sessionID = sessionService.logon(TEST_USER, TEST_PASSWORD);
+        sessionService.logoff(sessionID);
+    }
+    
+    
+    
+    
+    @Test
+    public void testRunQuery()
+    {
+        XmlViewService xmlViewServiceEndpoint = new XmlViewService();
+        XmlViewServiceSoap xmlViewService = xmlViewServiceEndpoint.getXmlViewServiceSoap();
+
+        SAWSessionService sessionServiceEndpoint = new SAWSessionService();
+        SAWSessionServiceSoap sessionService = sessionServiceEndpoint.getSAWSessionServiceSoap();
+        
+
+        String sessionID = sessionService.logon(TEST_USER, TEST_PASSWORD);
+        
+        ReportRef report = new ReportRef();
+        report.setReportPath("/shared/CCCi/SSW/SAI Donations");
+        
+        XMLQueryOutputFormat outputFormat = XMLQueryOutputFormat.SAW_ROWSET_SCHEMA_AND_DATA;
+        XMLQueryExecutionOptions executionOptions = new XMLQueryExecutionOptions();
+        executionOptions.setMaxRowsPerPage(-1);
+        executionOptions.setPresentationInfo(true);
+        
+        ReportParams reportParams = new ReportParams();
+        
+        QueryResults queryResults = xmlViewService.executeXMLQuery(
+            report, 
+            outputFormat, 
+            executionOptions, 
+            reportParams, 
+            sessionID);
+        
+        String rowset = queryResults.getRowset();
+
+        System.out.println(rowset);
+        sessionService.logoff(sessionID);
+    }
+    
+    
+    
+}

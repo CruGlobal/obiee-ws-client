e9d35fbbb47b4e4e69c46335a06bedd4da0f71d2
matthew.drees@uscm.org <matthew.drees@uscm.org@6185d61c-cf3b-044e-8ca2-f4aa9eb47c87>
2010-06-11 18:07:30 +0000

    add timeout handling
    git-svn-id: http://arkham.ccci.org/svn/java/trunk/commons/obiee-ws-client@6941 6185d61c-cf3b-044e-8ca2-f4aa9eb47c87

--- src/main/java/org/ccci/obiee/client/rowmap/impl/AnalyticsManagerFactoryImpl.java
+++ src/main/java/org/ccci/obiee/client/rowmap/impl/AnalyticsManagerFactoryImpl.java
@@ -1,5 +1,9 @@
 package org.ccci.obiee.client.rowmap.impl;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.xml.ws.BindingProvider;
+
 import org.ccci.obiee.client.rowmap.AnalyticsManager;
 import org.ccci.obiee.client.rowmap.AnalyticsManagerFactory;
 
@@ -9,6 +13,7 @@ import com.siebel.analytics.web.soap.v5.SAWSessionService;
 import com.siebel.analytics.web.soap.v5.SAWSessionServiceSoap;
 import com.siebel.analytics.web.soap.v5.XmlViewService;
 import com.siebel.analytics.web.soap.v5.XmlViewServiceSoap;
+import com.sun.xml.ws.client.BindingProviderProperties;
 
 public class AnalyticsManagerFactoryImpl implements AnalyticsManagerFactory
 {
@@ -19,6 +24,12 @@ public class AnalyticsManagerFactoryImpl implements AnalyticsManagerFactory
     private final String username;
     private final String password;
 
+    /** read timeout in ms.  Default is 30 seconds. */
+    private volatile int readTimeout = (int) TimeUnit.SECONDS.toMillis(30);
+    
+    /** connect timeout in ms.  Default is 4 seconds. */
+    private volatile int connectTimeout = (int) TimeUnit.SECONDS.toMillis(4);
+    
     public AnalyticsManagerFactoryImpl(SAWSessionService sawSessionService, XmlViewService xmlViewService, ReportEditingService reportEditingService, String username, String password)
     {
         this.sawSessionService = sawSessionService;
@@ -28,18 +39,54 @@ public class AnalyticsManagerFactoryImpl implements AnalyticsManagerFactory
         this.password = password;
     }
     
-
     public AnalyticsManager createAnalyticsManager()
     {
-        
         SAWSessionServiceSoap sawSessionServiceSoap = sawSessionService.getSAWSessionServiceSoap();
+        setTimeouts(sawSessionServiceSoap);
+        
         XmlViewServiceSoap xmlViewServiceSoap = xmlViewService.getXmlViewServiceSoap();
+        setTimeouts(xmlViewServiceSoap);
+        
         ReportEditingServiceSoap reportEditingServiceSoap = reportEditingService.getReportEditingServiceSoap();
+        setTimeouts(reportEditingServiceSoap);
         
         String sessionId = sawSessionServiceSoap.logon(username, password);
         
         ConverterStore converterStore = ConverterStore.buildDefault();
-        return new AnalyticsManagerImpl(sessionId, sawSessionServiceSoap, xmlViewServiceSoap, reportEditingServiceSoap, converterStore);
+        return new AnalyticsManagerImpl(
+            sessionId, 
+            sawSessionServiceSoap, 
+            xmlViewServiceSoap, 
+            reportEditingServiceSoap, 
+            converterStore);
+    }
+
+    private void setTimeouts(Object port)
+    {
+        BindingProvider bindingProvider = (BindingProvider) port;
+        bindingProvider.getRequestContext().put(BindingProviderProperties.CONNECT_TIMEOUT, connectTimeout);
+        bindingProvider.getRequestContext().put(BindingProviderProperties.REQUEST_TIMEOUT, readTimeout);
+    }
+
+
+    public int getReadTimeout()
+    {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(int readTimeout)
+    {
+        this.readTimeout = readTimeout;
+    }
+
+    public int getConnectTimeout()
+    {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(int connectTimeout)
+    {
+        this.connectTimeout = connectTimeout;
     }
     
 }
